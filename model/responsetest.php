7783e2c0698c45bdaee8d74a522c9ad7a795f2d2
haikaljs <haikaljavascript@gmail.com>
2023-10-17 22:27:02 +0800

    Create the response model

---
+++ model/responsetest.php
@@ -0,0 +1,18 @@
+<?php 
+
+require_once('Response.php');
+
+$response = new Response();
+$response->setSuccess(true);
+$response->setHttpStatusCode(200);
+$response->addMessage("Test Message 1");
+$response->addMessage("Test Message 2");
+$response->send();
+
+
+
+
+
+
+
+?>
