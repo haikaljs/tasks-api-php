dd6b4557e55d2c010a325e42a194b62b23814dba
haikaljs <haikaljavascript@gmail.com>
2023-10-18 16:05:32 +0800

    Delete a single task

--- controller/task.php
+++ controller/task.php
@@ -91,6 +91,38 @@ if(array_key_exists("taskid", $_GET)){
 
     }elseif($_SERVER['REQUEST_METHOD'] === 'DELETE'){
 
+        try{
+            $query = $writeDB->prepare('DELETE FROM tbltasks WHERE id = :taskid');
+            $query->bindParam(':taskid', $taskid, PDO::PARAM_INT);
+            $query->execute();
+
+            $rowCount = $query->rowCount();
+
+            if($rowCount === 0){
+                $response = new Response();
+                $response->setHttpStatusCode(404);
+                $response->setSuccess(false);
+                $response->addMessage('Task not found');
+                $response->send();
+                exit;
+            }
+
+            $response = new Response();
+            $response->setHttpStatusCode(200);
+            $response->setSuccess(true);
+            $response->addMessage("Task deleted");
+            $response->send();
+            exit();
+
+        }catch(PDOException $ex){
+            $response = new Response();
+            $response->setHttpStatusCode(404);
+            $response->setSuccess(false);
+            $response->addMessage('Failed ot delete task');
+            $response->send();
+            exit;
+        }
+
     }elseif($_SERVER['REQUEST_METHOD'] === 'PATCH'){
 
     }else{
